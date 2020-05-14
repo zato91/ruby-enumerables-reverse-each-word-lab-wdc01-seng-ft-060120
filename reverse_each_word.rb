a080ff82045e1264dc7f62058e63858e2d6def26
zara ahmadi <zatokey@gmail.com>
2020-05-14 21:28:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   
    string_arr = string.split
-   x = string_arr.map do |n|
-      text = n.reverse
-   end
+   x = string_arr.collect do |n|
+        n.reverse
+        end
   x.join(" ")
 end
